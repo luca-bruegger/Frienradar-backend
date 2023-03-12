4d4795dced8d4c8d001f57fdf7423c6b5b718c9c
Luca Brügger <hello@lucabruegger.ch>
2023-03-12 18:41:00 +0100

    Update github actions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['api.frienradar.com'] << ['localhost:8100', 'api.frienradar.com'] if Rails.env.development?
+    origins ['*.frienradar.com'] << 'localhost:8100' if Rails.env.development?
 
     resource '*',
              headers: :any,
