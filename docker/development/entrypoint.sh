f907769c6722c2f143b95ba14743601465ed1d2c
Luca Brügger <hello@lucabruegger.ch>
2023-03-16 11:55:58 +0100

    Update github actions

---
+++ docker/development/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+rm -f tmp/pids/server.pid
+
+echo "running bundle install"
+gem install bundler
+bundle install
+
+if [ -z "$SKIP_RAILS_MIGRATIONS" ]; then
+  echo "⚙️ Performing migrations"
+  bundle exec rails db:create db:migrate
+  echo "✅ Migrations done"
+fi
+
+if [ -z "$skip_seeds" ]; then
+  if [ ! -f /seed/done ]; then
+    echo "⚙️ Seeding db"
+    bundle exec rails db:seed && date > /seed/done
+    echo "✅ Seeding done"
+  else
+    echo "↪️ Skipping seeding because already done on $(cat /seed/done)"
+  fi
+fi
+
+echo "➡️ Handing control over to '$*''"
+
+echo "⚙️ Executing: $@"
+exec bundle exec "$@"
