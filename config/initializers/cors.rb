9b13d0a90de11fa3b667f8fbfb144e7bca7f76f0
Luca Brügger <hello@lucabruegger.ch>
2023-03-19 01:01:39 +0100

    Update docker setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if Rails.env.development? || Rails.env.staging?
-      origins ['*']
+    unless Rails.env.production?
+      origins ['http://localhost:8100']
       resource '*',
                headers: :any,
                expose: ['Authorization'],
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,14 +7,14 @@ rm -f tmp/pids/server.pid
 echo "⚙️ Installing gems for $RAILS_ENV environment"
 bundle install
 
-if [ -z "$SKIP_RAILS_MIGRATIONS" ]; then
+if [[ -z "$SKIP_RAILS_MIGRATIONS" ]]; then
   echo "⚙️ Performing migrations"
   bundle exec rails db:create db:migrate
   echo "✅ Migrations done"
 fi
 
-if [ -z "$skip_seeds" ]; then
-  if [ ! -f /seed/done ]; then
+if [[ -z "$skip_seeds" ]]; then
+  if [[ ! -f /seed/done ]]; then
     echo "⚙️ Seeding db"
     bundle exec rails db:seed && date > /seed/done
     echo "✅ Seeding done"
