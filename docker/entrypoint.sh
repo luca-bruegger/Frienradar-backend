aa011b1ca541e1d6216c7236d35383debac3341d
Luca Brügger <hello@lucabruegger.ch>
2023-03-19 21:13:07 +0100

    Update docker setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,19 +7,19 @@ rm -f tmp/pids/server.pid
 echo "⚙️ Installing gems for $RAILS_ENV environment"
 bundle install
 
-if [[ -z "$SKIP_RAILS_MIGRATIONS" ]]; then
+if [ -z "$SKIP_RAILS_MIGRATIONS" ]; then
   echo "⚙️ Performing migrations"
   bundle exec rails db:create db:migrate
   echo "✅ Migrations done"
 fi
 
-if [[ -z "$skip_seeds" ]]; then
-  if [[ ! -f /seed/done ]]; then
+if [ -z "$skip_seeds" ]; then
+  if [ ! -f /seed_done ]; then
     echo "⚙️ Seeding db"
-    bundle exec rails db:seed && date > /seed/done
+    bundle exec rails db:seed && date > /seed_done
     echo "✅ Seeding done"
   else
-    echo "↪️ Skipping seeding because already done on $(cat /seed/done)"
+    echo "↪️ Skipping seeding because already done on $(cat /seed_done)"
   fi
 fi
 
